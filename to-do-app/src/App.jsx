9483f369017321e7a036d10fc1077d49d6b22c51
AnfasArshad <anfasarshad@gmail.com>
2025-07-01 00:35:49 +0530

    Simple To Do App Using react vite completed

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  const addTodo = () => {
+    if (task.trim()) {
+      setTodos([...todos, { Text: task, completed: false }]);
+      setTask("");
+    }
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <>
+      <div className="app">
+        <h1>Todo List</h1>
+        <div className="input-section">
+          <input
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            placeholder="Add a new task"
+          />
+
+          <button onClick={addTodo}>Submit</button>
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index} className={todo.completed ? "completed" : ""}>
+              <span onClick={() => toggleTodo(index)}> {todo.Text} </span>
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
